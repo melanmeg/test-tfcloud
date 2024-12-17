ae4ca71899bba104d03bfb969acab475dfda6254
melanmeg <naoya05280708@gmail.com>
2024-12-18 01:33:12 +0900

    fix

---
+++ locals.tf
@@ -0,0 +1,5 @@
+locals {
+  project_id        = "test-project-373118"
+  region            = "asia-northeast1"
+  zones             = ["asia-northeast1-a"]
+}
--- main.tf
+++ main.tf
@@ -1,15 +1,29 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region = "ap-northeast-1"
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.1.0"
+    }
+  }
 }
 
-resource "aws_vpc" "test-vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "test-vpc"
+provider "google" {
+  project        = local.project_id
+  region         = local.region
+  zone           = local.zones[0]
+}
+
+resource "google_storage_bucket" "example_bucket" {
+  name          = "test-project-373118-sample-bucket-n"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30
+    }
   }
 }
